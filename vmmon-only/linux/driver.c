4dbb4d5f2f1dffb2a2ebbb9ff52d28a0285bc4fd
Elvis Dowson <elvis.dowson@gmail.com>
2026-07-27 16:44:30 -0400

    vmmon-only: Import fixes for building workstation-17.5.2 with linux kernel >= 6.16.0 This commit imports fixes for building workstation-17.5.2 with linux kernel >= 6.16.0 from https://github.com/aurelihein/vmware-host-modules/tree/workstation-17.6.4
    Signed-off-by: Elvis Dowson <elvis.dowson@gmail.com>

--- vmmon-only/linux/driver.c
+++ vmmon-only/linux/driver.c
@@ -345,7 +345,11 @@ LinuxDriverExit(void)
 
    Log("Module %s: unloaded\n", vmmon_miscdev.name);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 15, 4)
+   timer_delete_sync(&tscTimer);
+#else
    del_timer_sync(&tscTimer);
+#endif
 
    Vmx86_CleanupHVIOBitmap();
    Task_Terminate();
--- vmmon-only/linux/hostif.c
+++ vmmon-only/linux/hostif.c
@@ -1995,7 +1995,11 @@ HostIF_InitUptime(void)
 void
 HostIF_CleanupUptime(void)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 15, 4)
+   timer_delete_sync(&uptimeState.timer);
+#else
    del_timer_sync(&uptimeState.timer);
+#endif
 }
 
 
@@ -3410,7 +3414,11 @@ HostIF_SafeRDMSR(unsigned int msr,   // IN
    int err;
    u64 v;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 16, 0)
+   err = rdmsrq_safe(msr, &v);
+#else
    err = rdmsrl_safe(msr, &v);
+#endif
    *val = (err == 0) ? v : 0;  // Linux corrupts 'v' on error
 
    return err;
